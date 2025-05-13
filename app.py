4dcf80db2927670e7940cba161d2bdfe2123c611
DKMgaming <53993378+DKMgaming@users.noreply.github.com>
2025-05-13 10:08:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ from io import BytesIO
 # from python_docx import Document
 from docx import Document
 # Tải API Key từ môi trường (cập nhật nếu cần)
-API_KEY = os.getenv("AIzaSyAfQfOJgGCRxJyDMjr9Kv5XpBGTZX_pASQ")
 
+# API_KEY = os.getenv("AIzaSyAfQfOJgGCRxJyDMjr9Kv5XpBGTZX_pASQ")
+API_KEY = "AIzaSyAfQfOJgGCRxJyDMjr9Kv5XpBGTZX_pASQ"
 # Tạo client của Google GenAI
 def generate_transcription(file_path):
     # Khởi tạo client Google GenAI
